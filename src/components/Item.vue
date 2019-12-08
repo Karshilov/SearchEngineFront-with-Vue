3a5b2f07c59436a5b7e64b9cc92c7fd4126e2eb0
Karshilov <karshilov@foxmail.com>
2019-12-08 21:48:58 +0800

    small update

---
+++ src/components/Item.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class = "app-item">
+      <div class = "app-list">{{Name}}</div>
+        <div class = "app-attribute">{{Attr}}</div> 
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {"Name" : {type : String, default: ''}, "Attr" : {type : list, default: ''}}
+}
+</script>
+
+<style lang = 'less' scoped>
+.app-item {
+  position: absolute;
+}
+</style>
